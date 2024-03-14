9abaeb38bfc1ed9df68cdf4ffa1177219bf8f18b
khalil-touir <khaliltouir@live.fr>
2024-03-14 01:20:35 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,59 @@
 import * as React from "react";
+
 const title = "React";
+
 const list = [
   {
-    title: "React ",
+    title: "React",
     url: "https://reactjs.org/",
-    author:"Jordan Walke",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
-    title :"Redux",
+    title: "Redux",
     url: "https://redux.js.org/",
-    author:"Dan Abramov, Andrew Clarck",
-    num_comments:2,
+    author: "Dan Abramov, Andrew Clarck",
+    num_comments: 2,
     points: 5,
     objectID: 1,
   },
 ];
-function App() {
-  
 
+const App = () => (
+  <div>
+    <h1>My Hacker Stories </h1>
+    <Search />
+    <List />
+  </div>
+);
+
+const Search = () => {
+  const handleChange = (event) =>{
+    console.log(event.target.value);
+  };
   return (
     <div>
-     <h1>My Hacker Stories </h1>
-     <label htmlFor="search">Search: </label>
-     <input id="search" type="text" /> 
-    <ul>
-    {list.map(function(item) {
-      return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        );
-      })}
-      
-      </ul> 
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
     </div>
-
-   
   );
-}
+};
 
-export default App
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
+);
+
+export default App;
