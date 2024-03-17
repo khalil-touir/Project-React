dbe481ef44a66bef9a05ce9525f1801901ea7aa6
khalil-touir <khaliltouir@live.fr>
2024-03-17 18:29:38 +0100

    modifier app.jsx , ajouter deux nouveau fichier list.jsx et search.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,41 @@
 import * as React from "react";
+import List from './List';
+import Search from "./Search";
 
-const title = "React";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clarck",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const App = () => (
-  <div>
-    <h1>My Hacker Stories </h1>
-    <Search />
-    <List />
-  </div>
-);
-
-const Search = () => {
-  const handleChange = (event) =>{
-    console.log(event.target.value);
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clarck",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  const [searchTerm,setSearchTerm] = React.useState('');
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
   };
   return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-    </div>
-  );
-};
+  <div>
+    <h1>My Hacker Stories </h1>
 
-const List = () => (
-  <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </li>
-    ))}
-  </ul>
-);
-
+    <Search onSearch={handleSearch} />
+    <p>
+      Searching for <strong>{searchTerm}</strong>
+      </p>
+    <hr />
+    <List list ={stories} />
+  </div>
+  );};
 export default App;
---
+++ src/Search.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+const Search = (props) => (
+  
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={props.onSearch} />
+     
+    </div>
+  );
+export default Search;  
